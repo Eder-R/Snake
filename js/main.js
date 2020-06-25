b2212eb1e8519d12dd9c17118642926df766138c
Eder-R <eder.ramos@catolicasc.org.br>
2020-06-25 18:11:59 -0300

    Refeito do zero Absoluto

--- js/main.js
+++ js/main.js
@@ -1,185 +1,100 @@
-	var canvas, ctx, WIDTH, HEIGHT, FPS, tileSize, playing;
-	var snake, playLabel;
-	var globalTouch = [], offset = [];
+window.onload = function () {
 
-	var keys = {
-		left: 37,
-		up: 38,
-		right: 39,
-		down: 40
-	}
-		//Functions Mobile
-	window.addEventListener("touchstart", touchStart);
-	window.addEventListener("touchmove", touchMove);
-	window.addEventListener("touchend", touchEnd);
+	var stage = document.getElementById('stage');
+	var ctx = stage.getContext("2d");
+	document.addEventListener("keydown",keypush);
+	setInterval(game, 60);
 
-		//Function Desktop (PCs e Notebooks)
-	window.addEventListener("keydown", keyDown);
+	const vel = 1;
 
-		//Function Responsive
-	window.addEventListener("resize", resizeWindow);
+	var vx = vy = 0;
 
-	function touchEnd(e) {
-		if (Math.abs(offset[0]) > Math.abs(offset[1]))
-			snake.direction = [offset[0] / Math.abs(offset[0]), 0];
+	//TAM cobra
+	var px = 10;
+	var py = 15;
 
-		else
-			snake.direction = [0, offset[1] / Math.abs(offset[1])];
-	}
+	//TAM && Quantidade de peças
+	var tp = 20;
+	var qp = 20;
 
-	function touchMove(e) {
-		var touch = e.touches [0];
+	//tamanho da maçã
+	var ax = 15;
+	var ay = 15;
 
-		offset = [touch.pageX - globalTouch[0], touch.pageY - globalTouch[1]];
-	}
+	var trail = [];
+	tail = 5;
 
-	function touchStart (e) {
-		e.preventDefault();
-
-		var touch = e.touches[0]
-		globalTouch = [touch.pageX, touch.pageY];
-
-	}
-
-	function keyDown (e) {
-
-		if (!playing && (e.keyCode == keys.up || e.keyCode == keys.left || e.keyCode == keys.down || e.keyCode == keys.right))
-			playing = true
-
-		switch (e.keyCode){
-			case keys.left:
-				snake.direction = [-1, 0];
-				break;
-
-			case keys.up:
-				snake.direction = [0, -1];
-				break;
-
-			case keys.right:
-				snake.direction = [1, 0]
-				break;
-
-			case keys.down:
-				snake.direction = [0, 1];
-				break;
+	function game() {
+		px += vx;
+		py += vy;
+		if (px < 0) {
+			px = qp - 1;
 		}
-	}
-
-	function resizeWindow() {
-		WIDTH = window.innerWidth;
-		HEIGHT = window.innerHeight;
-
-		canvas.width = WIDTH;
-		canvas.height = HEIGHT;
-
-		tileSize = Math.max(Math.floor(WIDTH / 60), Math.floor(HEIGHT / 60));
-	}
-
-	function isMobileDevice(){
-		return /Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent) 
-	}
-
-	function init() {
-		canvas = document.createElement("canvas");
-		resizeWindow();
-		document.body.appendChild(canvas);
-		ctx = canvas.getContext("2d");
-
-		FPS = 15;
-
-		newGame();
-		run();
-
-	};
-
-	function newGame() {
-		snake = new Snake();
-		playLabel = new PlayLabel();
-
-		playing = false;
-	}
-
-	function PlayLabel() {
-		this.text;
-		this.color = "#5d8357";
-		this.messages = {
-			portrait:"Rotacione o dispositivo para poder jogar!",
-			landscape:"Arraste a tela para jogar!",
-			pc:"Pressione as setas para jogar"
+		if (px > qp - 1) {
+			px = 0;
+		}
+		if (py < 0) {
+			py = qp - 1;
+		}
+		if (py > qp - 1) {
+			py = 0;
 		}
 
-		if (isMobileDevice()) {
+		//FUNDO DO JOGO
+		ctx.fillStyle = "black";
+		ctx.fillRect(0, 0, stage.width, stage.height);
+		//FRUTA / MAÇÃ
+		ctx.fillStyle = "Red";
+		ctx.fillRect(ax * tp, ay * tp, tp, tp);
+		//RASTRO DA COBRA
+		ctx.fillStyle = "gray";
+		for (var i = 0; i < trail.length; i++) {
+			ctx.fillRect(trail[i].x * tp, trail[i].y * tp, tp, tp);
 
-		} else {
-			this.text = this.messages["pc"]
-		}
-
-		this.draw = function () {
-			ctx.fillStyle = this.color;
-			ctx.font = tileSize * 2 + "px Arial";
-			ctx.fillText(this.text, WIDTH / 2 - ctx.measureText(this.text).width / 2 ,HEIGHT / 2);
-		}
-
-	}
-
-	function Snake () {
-		this.body = [[10, 10], [10, 11], [10, 12]];
-		this.color = "#000"
-		this.direction = [0, -1];
-
-		this.update = function() {
-			var nextPos = [this.body[0][0] + this.direction[0], this.body[0][1] + this.direction[1]]
-			if (!playing) {
-				if (this.direction[1] == -1 && nextPos[1] <= (HEIGHT * 0.1 / tileSize))
-					this.direction = [1, 0];
-
-					else if (this.direction[0] == 1 && nextPos[0] >= (WIDTH * 0.9 / tileSize))
-					this.direction = [0, 1];
-
-					else if (this.direction[1] == 1 && nextPos[1] >= (HEIGHT * 0.9 / tileSize))
-					this.direction = [-1, 0];
-
-					else if (this.direction[0] == -1 && nextPos[0] <= (WIDTH * 0.1 / tileSize))
-					this.direction = [0, -1];
-
-			};
-
-			if (nextPos[0] == this.body[1][0] && nextPos[1] == this.body[1][1]) {
-				this.body.reverse();
-				nextPos = [this.body[0][0] + this.direction[0], this.body[0][1] + this.direction[1]]
-			}
-
-			this.body.pop();
-			this.body.splice(0, 0, nextPos);
-		}
-
-		this.draw = function() {
-			ctx.fillStyle = this.color
-
-			for (var i = 0; i < this.body.length; i++) {
-				ctx.fillRect(this.body[i][0] * tileSize, this.body[i][1] * tileSize, tileSize, tileSize)
+			if (trail[i].x == px && trail[i].y == py) {
+				vx = vy = 0;
+				tail = 5;
 			}
 		}
+
+		trail.push({
+			x: px,
+			y: py
+		})
+		while (trail.length > tail) {
+			trail.shift();
+		}
+
+		if (ax == px && ay == py) {
+			tail++;
+			ax = Math.floor(Math.random() * qp)
+			ay = Math.floor(Math.random() * qp)
+		}
+
 	}
 
-	function update() {
-		snake.update();
+	function keypush(event) {
+		switch (event.keyCode) {
+			case 37:	//SETA ESQUERDA
+				vx = -vel;
+				vy= 0;
+				break;
+			case 38:	//SETA CIMA
+				vx = 0;
+				vy= -vel;
+				break;
+			case 39:	//SETA DIREITA
+				vx = vel;
+				vy= 0;
+				break;
+			case 40:	//SETA BAIXO
+				vx = 0;
+				vy= vel;
+				break;
 
-	};
+			default:
+				break;
+		}
 
-	function run() {
-		update();
-		draw();
-
-		setTimeout(run, 1000 / FPS)
-	};
-
-	function draw() {
-		ctx.clearRect(0, 0, WIDTH, HEIGHT);
-
-		snake.draw();
-		if (!playing)
-			playLabel.draw();
-	};
-
-	init();
+	}
+}
